2eaaad56e16c7bc25a1c7a78420fe9edeca6b565
GunukulaNiharika <niharikagunukula2002@gmail.com>
2021-04-27 16:01:30 +0530

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -173,9 +173,7 @@ module.exports.forgotpassword_post= async (req,res)=>{
      if(user!=null){
       const token=jwt.sign({_id:user._id},process.env.RESET_PASSWORD_KEY,{expiresIn: '10m'});
         var transporter= nodemailer.createTransport({
-          host: 'smtp.gmail.com',
-          port:465,
-          secure:true,
+          service: 'gmail',
           auth: {
             user: 'hackthis404@gmail.com',
             pass: 'hackifucan404'
