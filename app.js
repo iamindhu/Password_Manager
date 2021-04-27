4abccf548869dcf1c735c8908b6540f30653b8af
GunukulaNiharika <niharikagunukula2002@gmail.com>
2021-04-27 13:23:39 +0530

    updated

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs');
 
 // database connection
 mongoose.connect(process.env.dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+  .then((result) => app.listen(process.env.PORT))
   .catch((err) => console.log(err));
 
 // routes
